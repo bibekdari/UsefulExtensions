f5915bb6c5074721989f9b6cae7f90c259a14112
Bibek Sharma Timalsina <timalsina.bibek@gmail.com>
2018-08-22 00:13:12 +0545

    Create SequenceExtension.swift

---
+++ SequenceExtension.swift
@@ -0,0 +1,25 @@
+//
+//  SequenceExtension.swift
+//  Tsukasa
+//
+//  Created by shishir sapkota on 10/9/17.
+//  Copyright © 2017 Bibek Timalsina. All rights reserved.
+//
+
+import Foundation
+
+public extension Sequence {
+
+    func group<U: Hashable>(by key: (Iterator.Element) -> U) -> [U:[Iterator.Element]] {
+        var categories: [U: [Iterator.Element]] = [:]
+        for element in self {
+            let key = key(element)
+            if case nil = categories[key]?.append(element) {
+                categories[key] = [element]
+            }
+        }
+        return categories
+    }
+   
+}
+
